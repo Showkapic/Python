d5f38fcccaf46bcea546fe5c45c1d959837a30b6
Leo Machtelinckx <Showkapiic@gmail.com>
2025-11-07 08:41:16 +0100

    a

--- Python Jour 7-8/Job4.py
+++ Python Jour 7-8/Job4.py
@@ -1,3 +1,7 @@
-def code(message,decalage):
-    for letter in message:
-        
+def cesar(message, k):
+    return "".join(
+        chr((ord(c) - (o:=ord('a' if c.islower() else 'A')) + k) % 26 + o)
+        if c.isalpha() else c
+        for c in message
+    )
+cesar("je suis un codeur",3)
---
+++ Python Jour 7-8/Job5.py
@@ -0,0 +1,4 @@
+def trajet(marche,H):
+    resultat=(marche*H)*5*7
+    print(resultat)
+trajet(50,25)
